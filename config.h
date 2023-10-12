8b6a258a5e110bd1f47396c861abbb5d9c4652ea
Alexander Bocken <alexander@bocken.org>
2023-10-12 16:28:21 +0200

    reduce default font-size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=32:antialias=true:autohint=true";
-static char *font2[] = { "monospace:pixelsize=32:antialias=true:autohint=true" };
+static char *font = "monospace:pixelsize=24:antialias=true:autohint=true";
+static char *font2[] = { "monospace:pixelsize=24:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
